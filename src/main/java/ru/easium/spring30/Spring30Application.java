6911d5b6c22531c60e5d3a7469b29ad8e0b4b1e3
savok <estonia>
2020-05-18 05:31:55 +0300

    Initial commit

---
+++ src/main/java/ru/easium/spring30/Spring30Application.java
@@ -0,0 +1,13 @@
+package ru.easium.spring30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class Spring30Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring30Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/easium/spring30/Spring30ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.easium.spring30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring30ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
